7dc6081086e5d8c13ca7863683f96dc31625f45a
Jon Meyers <jonathon.d.meyers@gmail.com>
2022-05-24 10:54:18 +1000

    remove unused supabase function

--- next-js/supabase/functions/_utils/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@^1.33.2";
-
-const supabase = createClient(
-  Deno.env.get("SUPABASE_URL") ?? "",
-  Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ?? ""
-);
-
-export default supabase;

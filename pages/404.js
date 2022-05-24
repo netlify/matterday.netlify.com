edc6f17316a16aa3d0d836c013eda1d2561e809f
Lynn Fisher <lynn.fisher@netlify.com>
2022-05-24 10:31:22 -0700

    whoops

--- pages/404.js
+++ pages/404.js
@@ -6,7 +6,7 @@ export default function FourOhFour() {
     <div className="four-oh-four">
       <header className="header-main">
         <div>
-         <Link a href="/"><a className="masthead">Matterday</a></Link>
+         <Link href="/"><a className="masthead">Matterday</a></Link>
         </div>
         <a href="https://netlify.com" className="logo">
           <img src="/images/logomark-netlify.svg" width="26" height="26" alt="Netlify logomark" />
@@ -18,7 +18,7 @@ export default function FourOhFour() {
       <section className="content">
         <div className="container">
           <h1>404 - Page Not Found</h1>
-         <Link a href="/"><a>Head back home</a></Link>
+         <Link href="/"><a>Head back home</a></Link>
         </div>
       </section>
     </div>

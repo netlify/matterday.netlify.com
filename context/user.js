0e2175a2424571ce1386d147ba3c927c3f54e206
Jon Meyers <jonathon.d.meyers@gmail.com>
2022-05-25 10:05:33 +1000

    redirect signed in user to share form

--- context/user.js
+++ context/user.js
@@ -1,16 +1,24 @@
 import { createContext, useState, useEffect, useContext } from "react";
 import supabase from "../utils/supabase";
+import { useRouter } from "next/router";
 
 const Context = createContext();
 
 const Provider = ({ children }) => {
+  const router = useRouter();
   const [user, setUser] = useState(supabase.auth.user());
 
   useEffect(() => {
     setUser(supabase.auth.user());
 
     supabase.auth.onAuthStateChange(() => {
-      setUser(supabase.auth.user());
+      const newUser = supabase.auth.user();
+      setUser(newUser);
+
+      if (newUser && router.pathname === "/") {
+        // the user has just signed in and is on the homepage - navigate them to the share form
+        router.push("/#share");
+      }
     });
   }, []);
 

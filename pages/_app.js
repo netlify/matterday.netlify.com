b3673c91324efb1b5b0913f4453b0c4628ef69b6
Lynn Fisher <lynn.fisher@netlify.com>
2022-05-25 15:46:52 -0700

    update gta ID

--- pages/_app.js
+++ pages/_app.js
@@ -48,10 +48,10 @@ function MyApp({ Component, pageProps }) {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-WNHHKD2');
+          })(window,document,'script','dataLayer','GTM-T7WNFLD');
         `}
       </Script>
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WNHHKD2" height="0" width="0" style={{display: "none", visibility: "hidden"}}></iframe></noscript>
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T7WNFLD" height="0" width="0" style={{display: "none", visibility: "hidden"}}></iframe></noscript>
       <UserProvider>
         <Component {...pageProps} />
       </UserProvider>

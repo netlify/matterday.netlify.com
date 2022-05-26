729b2e9644ff1254e0ece8b6110f69e2fbdc197f
Lynn Fisher <lynn.fisher@netlify.com>
2022-05-26 09:57:16 -0700

    put GTM in the head, #22

--- pages/_app.js
+++ pages/_app.js
@@ -41,8 +41,17 @@ function MyApp({ Component, pageProps }) {
         <meta content="@netlify" name="twitter:creator" />
         <meta content="Matterday" name="twitter:title" property="og:title" />
         <meta content="Join the Netlify community in celebrating what matters." name="twitter:description" property="og:description" />
+        <script
+          dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','GTM-T7WNFLD');`,
+          }}
+        />
       </Head>
-      <Script id="google-tag-manager" strategy="afterInteractive">
+      {/*<Script id="google-tag-manager" strategy="afterInteractive">
         {`
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -50,7 +59,7 @@ function MyApp({ Component, pageProps }) {
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-T7WNFLD');
         `}
-      </Script>
+      </Script>*/}
       <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T7WNFLD" height="0" width="0" style={{display: "none", visibility: "hidden"}}></iframe></noscript>
       <UserProvider>
         <Component {...pageProps} />
